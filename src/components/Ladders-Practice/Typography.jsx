79d6db99b633a74e5bfc67a74684dca7dbfccb1b
Madlad <72143774+Sahaj-Srivastava24@users.noreply.github.com>
2022-01-20 03:37:28 +0530

    problem pages implemented

---
+++ src/components/Ladders-Practice/Typography.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import img from '../../assets/topicwise/trello.png'
+
+const basePositionStyles = {
+  display: 'flex',
+  margin: '1rem 5rem',
+}
+
+export const baseTypographyStyles = {
+  fontFamily: 'Inter, sans-serif',
+  color: '#fff',
+}
+
+const headingStyles = {
+  ...baseTypographyStyles,
+  fontSize: '2rem',
+  fontWeight: 'bold',
+  marginBottom: '0',
+  lineHeight: '3rem',
+  paddingTop: '1rem',
+}
+
+const subHeadingStyles = {
+  ...baseTypographyStyles,
+  fontSize: '0.7rem',
+  fontWeight: 'bold',
+  width: '100%',
+}
+
+function Typography({ heading, subheading }) {
+  return (
+    <div style={basePositionStyles}>
+      <div>
+        <img src={img} alt="" height={120} width={120} />
+      </div>
+      <div style={{ margin: '0 0 0 1rem' }}>
+        <h1 style={headingStyles}>
+          {heading}
+          <span style={{ color: '35AE9A' }}>.</span>
+        </h1>
+        <p style={subHeadingStyles}>{subheading}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Typography
--- src/pages/Ladders-Practice/Parent.js
+++ src/pages/Ladders-Practice/Parent.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Space, Row, Col } from 'antd'
+import ParentCard from '../../components/Ladders-Practice/ParentCard'
+import Typography from '../../components/Ladders-Practice/Typography'
+import { cards as data, pages } from '../../components/Ladders-Practice/data'
+
+const globalStyles = {
+  color: '#fff',
+  margin: '0 20vw',
+}
 
 function Parent(props) {
-  console.log(props)
-  return <div>Page with Levels and Topics</div>
+  const { type, wise } = props
+  console.log(type, wise)
+  return (
+    <>
+      <Space style={globalStyles} direction="vertical">
+        <Row>
+          <Typography
+            heading={pages[0].heading}
+            subheading={pages[0].subheading}
+          />
+        </Row>
+        <Row>
+          {data.map((item, index) => {
+            return (
+              <Col span={8} key={index}>
+                <ParentCard
+                  title={item.title}
+                  description={item.des}
+                  numberOfPeople={item.numberOfPeople}
+                  completed={item.questionsDone}
+                  total={item.questionsTotal}
+                />
+              </Col>
+            )
+          })}
+        </Row>
+      </Space>
+    </>
+  )
 }
 
 export default Parent
